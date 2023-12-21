b2edb5c8da86a84f0c262a8a46fd77d9f39b8aac
moustafa ascoura <moustafaascoura@gmail.com>
2023-12-21 02:45:52 +0200

    request headers

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+app.get('/api/whoami', (req, res) => {
+    res.json({"ipaddress":req.ip,
+    "language":req.headers['accept-language'],
+    "software":req.headers['user-agent']})
+})
+
 app.get('/api/:date?', (req, res) => {
     let input = req.params.date
     input = String(parseInt(input)) === input ? parseInt(input) : input
@@ -19,6 +25,8 @@ app.get('/api/:date?', (req, res) => {
 
     
 })
+
+
 
 const listener = app.listen(3000, () => {
     console.log('Server is listening on port 3000 ...')
