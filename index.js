06dd81a7886abfcbbd02f8e96c0708a99d4c90cb
moustafa ascoura <moustafaascoura@gmail.com>
2023-12-21 02:37:19 +0200

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
 app.get('/api/:date?', (req, res) => {
     let input = req.params.date
     input = String(parseInt(input)) === input ? parseInt(input) : input
